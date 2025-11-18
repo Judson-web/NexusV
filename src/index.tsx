1fa199219d88398a84bf94831eabb5d28c4f278f
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2025-11-18 19:53:04 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
+// src/index.tsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-const root = createRoot(document.getElementById("root")!);
-root.render(<App />);
+const rootEl = document.getElementById("root");
+
+if (rootEl) {
+  const root = createRoot(rootEl);
+  root.render(<App />);
+}
