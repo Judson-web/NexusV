23bb1e68351b85d17a910cdd8d1175d591e442fe
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2025-11-18 18:57:07 +0530

    Create types.tsx

---
+++ src/types.tsx
@@ -0,0 +1,10 @@
+export type VideoItem = {
+  id: string;
+  title: string;
+  channel: string;
+  views: number;
+  uploadedAt: string;
+  src: string;
+  thumbnail?: string;
+  duration?: string;
+};
